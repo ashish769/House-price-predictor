f6d8a5d16fcd8c5ddc1f8d6b3a52701b15a8dff1
ashish <ashishrajpoudel28@gmail.com>
2025-04-06 15:05:42 +0545

    creating the server which can predict the home prices

--- server/server.py
+++ server/server.py
@@ -1,9 +1,25 @@
 from flask import Flask,request,jsonify
+import util
 app = Flask(__name__)
+util.load_saved_artifacts()
+@app.route('/loc')
+def get_locations_names():
+   location=jsonify({
+       'locations':util. get_locations_names()
+   }) 
+   return location
+@app.route('/price',methods=['POST'])
+def predict_home_price():
+    total_sqft=float(request.form['total_sqft'])
+    location=request.form['location']
+    bhk=int(request.form['bhk'])
+    bath=int(request.form['bath'])
 
-@app.route('/hello')
-def Hello():
-    return "Hi"
+    response=jsonify({
+        'estimated_price':util.get_estimated_price(location,total_sqft,bhk,bath)
+    })
+
+    return response
 
 if __name__ == "__main__":
     print("running the server")
